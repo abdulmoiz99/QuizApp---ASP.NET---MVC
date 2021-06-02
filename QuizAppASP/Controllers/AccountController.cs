e5fbf17a019d0f1d6cd00b8f6c5f924db06acd44
Abdul Moiz <moiza0799@gmail.com>
2021-06-02 22:25:58 +0500

    Login Page Added

---
+++ QuizAppASP/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using QuizAppASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuizAppASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void ConnectionString()
+        {
+            con.ConnectionString = @"Data Source=ABDUL-MOIZ\SQLEXPRESS;Initial Catalog=TestDatabae;Integrated Security=True;Pooling=False";
+        }
+        [HttpPost]
+        public ActionResult Verify(Account account)
+        {
+            ConnectionString();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from login where Name = '" + account.email + "' and Password = '" + account.Password + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("LoginError");
+            }
+        }
+    }
+}
---
+++ QuizAppASP/resources/js/javascript.js
@@ -0,0 +1,16 @@
+function Animate() {
+    document.getElementById("about-element").style.margin = "200px auto 30px auto";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+  function showPopup()
+  {
+    document.getElementById("ScorePopup").style.display = "block";
+  }
+  function closePopup()
+  {
+    document.getElementById("ScorePopup").style.display = "none";
+  }
