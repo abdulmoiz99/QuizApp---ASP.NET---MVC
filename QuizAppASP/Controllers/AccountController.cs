d506575be9163473e50d40710a820a675e55ac7b
Abdul Moiz <moiza0799@gmail.com>
2021-06-03 13:18:31 +0500

    Register User Page Added

--- QuizAppASP/Controllers/AccountController.cs
+++ QuizAppASP/Controllers/AccountController.cs
@@ -10,7 +10,7 @@ namespace QuizAppASP.Controllers
 {
     public class AccountController : Controller
     {
-       
+
         [HttpGet]
         public ActionResult Login()
         {
@@ -27,14 +27,14 @@ namespace QuizAppASP.Controllers
         [HttpPost]
         public ActionResult Verify(Account account)
         {
-            if (Main.verifyUser(account.Email,account.Password))
+            if (Main.verifyUser(account.Email, account.Password))
             {
                 return View("~/Views/Home/Homepage.cshtml");
 
             }
             else
             {
-               return View("Register");
+                return View("Register");
             }
         }
         [HttpPost]
@@ -44,6 +44,30 @@ namespace QuizAppASP.Controllers
                                       " values('" + account.FirstName + "','" + account.LastName + "','" + account.Email + "','" + account.Password + "',1 )";
             SQL.NonScalarQuery(query);
             return View("Login");
+        }
+        public ActionResult Users(Account account)
+        {
+            string con = @"Data Source=ABDUL-MOIZ\SQLEXPRESS;Initial Catalog=QuizAppAsp;Integrated Security=True;Pooling=False";
+            String sql = "SELECT * FROM Login";
+            SqlCommand cmd = new SqlCommand(sql, SQL.Con);
+
+            var model = new List<Account>();
+            if (SQL.Con.State == System.Data.ConnectionState.Open)
+            {
+                SQL.Con.Close();
+            }
+            SQL.Con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var users = new Account();
+                users.FirstName = rdr["LoginFirstName"].ToString();
+                users.LastName = rdr["LoginLastName"].ToString();
+                users.Email = rdr["LoginEmail"].ToString();
+
+                model.Add(users);
+            }
+            return View(model);
         }
     }
 }
